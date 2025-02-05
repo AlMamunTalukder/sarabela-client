b6a426f7818581632912a8a10456455d94924ae2
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2025-02-05 12:18:38 +0600

    newscard-seperate

--- src/components/LeadNews/LeadNewsCard.tsx
+++ src/components/LeadNews/LeadNewsCard.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-"use client";
-import Image from "next/image";
+import React from 'react';
 import top_news from "@public/asset/bangladesh/bangladesh-top.jpg";
 import economy from "@public/asset/bangladesh/economy.jpg";
 import environment from "@public/asset/bangladesh/environment.jpg";
 import sport from "@public/asset/bangladesh/sport.jpg";
 import Link from "next/link";
 import { useGetAllNewsQuery } from "@/redux/dailynews/news.api";
-import LeadNewsCard from "./LeadNewsCard";
+import Image from 'next/image';
 
 
 
@@ -69,80 +68,45 @@ const bangladesh_news_data = [
   },
 ];
 
-const NewsCard = () => {
-
-  const { data, isLoading, isError } = useGetAllNewsQuery({});
-
-  const leadNews = bangladesh_news_data.find(
-    (news) => news.news_type === "lead"
-  );
-  const normalNews = bangladesh_news_data.filter(
-    (news) => news.news_type === "normal"
-  );
-
-  return (
-    <div className="flex flex-col gap-4">
-      {/* Trending News Section */}
-      {leadNews && (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          {/* Lead News */}
-          <div className="lg:col-span-2">
-            <Link href="/view_details" className="block group">
-              <div className="relative aspect-[4/3] overflow-hidden">
-                <div className="relative w-full h-full transform transition-transform duration-500 group-hover:scale-110">
-                  <Image
-                    src={leadNews.image}
-                    alt={leadNews.title}
-                    placeholder="blur"
-                    className="object-cover"
-                    fill
-                  />
-                </div>
-                <div className="absolute bottom-0 p-4 bg-gradient-to-t from-black to-transparent w-full">
-                  <h1 className="text-2xl lg:text-4xl text-white font-semibold group-hover:text-yellow-500 transition-colors">
-                    {leadNews.title}
-                  </h1>
-                </div>
+const LeadNewsCard = () => {
+    
+    const {data, isLoading, isError} = useGetAllNewsQuery({}); 
+    
+      const leadNews = bangladesh_news_data.find(
+        (news) => news.news_type === "lead"
+      );
+      const normalNews = bangladesh_news_data.filter(
+        (news) => news.news_type === "normal"
+      );
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {normalNews.slice(0, 3).map((news) => (
+          <Link
+            key={news.id}
+            href="/view_details"
+            className="group flex lg:flex-col flex-row gap-4"
+          >
+            <div className="w-full overflow-hidden">
+              <div className="relative aspect-[3/2] transform transition-transform duration-500 hover:scale-105">
+                <Image
+                  src={news.image}
+                  alt={news.title}
+                  placeholder="blur"
+                  className="object-cover"
+                  fill
+                />
               </div>
-            </Link>
-          </div>
-
-          {/* Side News */}
-          <div className="hidden lg:flex flex-col gap-4 border-s border-gray-500 ps-2">
-            {normalNews.map((news) => (
-              <Link
-                key={news.id}
-                href="/view_details"
-                className="group flex flex-row-reverse gap-2 border-b  border-gray-200 p-1"
-              >
-                <div className="w-full overflow-hidden">
-                  <div className="relative aspect-[3/2] transform transition-transform duration-500 hover:scale-105">
-                    <Image
-                      src={news.image}
-                      alt={news.title}
-                      placeholder="blur"
-                      className="object-cover"
-                      fill
-                    />
-                  </div>
-                </div>
-                <div className="w-full ps-2 lg:pt-2">
-                  <h2 className="text-xl font-bold hover:text-blue-600 transition-colors">
-                    {news.title}
-                  </h2>
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div>
-      )}
-
-      <div className="border-t border-gray-500" />
-
-      {/* Bottom News Grid */}
-      <LeadNewsCard />
-    </div>
-  );
+            </div>
+            <div className="w-full ps-2 lg:pt-2">
+              <h2 className="text-xl font-bold hover:text-blue-600 transition-colors">
+                {news.title}
+              </h2>
+              <p className="text-sm text-gray-600 mt-2">{news.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
 };
 
-export default NewsCard;
+export default LeadNewsCard;
